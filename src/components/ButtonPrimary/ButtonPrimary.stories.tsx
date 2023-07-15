a2e5fd7150abb1f67a502ea01d980e544c7b3f78
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-07-15 17:01:48 -0300

    feat: button primary tests & documentation in the storybook

---
+++ src/components/ButtonPrimary/ButtonPrimary.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Button from "./index";
+import { ButtonProps } from "./ButtonProps";
+
+export default {
+  title: "components/shared/ButtonPrimary",
+  component: Button,
+  tags: ["autodocs"],
+  args: {
+    label: "Button",
+    disabled: false,
+    type: "button",
+  },
+} as Meta<ButtonProps>;
+
+export const Primary: StoryObj = {};
+
+export const Disabled: StoryObj = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const Small: StoryObj = {
+  args: {
+    maxWidth: "120px",
+  },
+};
---
+++ src/components/ButtonPrimary/ButtonPrimary.test.tsx
@@ -0,0 +1,24 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import ButtonPrimary from ".";
+
+describe("Button Primary", () => {
+  it("should render correctly with props being passed", () => {
+    const onClickFn = jest.fn();
+
+    render(
+      <ButtonPrimary
+        disabled={false}
+        label="Click"
+        type="button"
+        data-testid="button-id"
+        onClick={onClickFn}
+      />
+    );
+
+    const button = screen.getByTestId("button-id");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveAttribute("type");
+    fireEvent.click(button);
+    expect(onClickFn).toHaveBeenCalled();
+  });
+});
