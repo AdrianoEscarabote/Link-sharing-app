a49ca6125c3247dda9cad047b48c9dd6b42b6742
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-07-15 17:02:32 -0300

    feat: button secondary tests & documentation in the storybook

---
+++ src/components/ButtonSecondary/Button.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { ButtonProps } from "./ButtonProps";
+import ButtonSecondary from ".";
+
+export default {
+  title: "components/shared/ButtonSecondary",
+  component: ButtonSecondary,
+  tags: ["autodocs"],
+  args: {
+    label: "Button",
+    disabled: false,
+  },
+} as Meta<ButtonProps>;
+
+export const Primary: StoryObj = {};
+
+export const Disabled: StoryObj = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const Small: StoryObj = {
+  args: {
+    maxWidth: "120px",
+  },
+};
---
+++ src/components/ButtonSecondary/ButtonSecondary.test.tsx
@@ -0,0 +1,24 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import ByttonSecondary from "./index";
+
+describe("Button Secondary", () => {
+  it("should render correctly with props being passed", () => {
+    const onClickFn = jest.fn();
+
+    render(
+      <ByttonSecondary
+        disabled={false}
+        label="Click"
+        type="button"
+        data-testid="button-id"
+        onClick={onClickFn}
+      />
+    );
+
+    const button = screen.getByTestId("button-id");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveAttribute("type");
+    fireEvent.click(button);
+    expect(onClickFn).toHaveBeenCalled();
+  });
+});
