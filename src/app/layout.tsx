b0b8c5714d74f0916fbb687a69f81e6eeb66d0e6
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-07-15 17:00:16 -0300

    feat: layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Head from "next/head";
+import "./styles/global.css";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=7" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Instrument+Sans:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Provider store={store}>{children}</Provider>
+      </body>
+    </html>
+  );
+}
