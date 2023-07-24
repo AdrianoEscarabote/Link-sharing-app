e8d6a9dadaade1176d524f2b308c7c09c97d679a
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-07-24 11:27:18 -0300

    feat: preview link component

---
+++ src/components/PreviewLink/LinkTypes.d.ts
@@ -0,0 +1,40 @@
+import { ComponentPropsWithoutRef } from "react";
+
+interface LinkTypes extends ComponentPropsWithoutRef<"a"> {
+  link: string;
+  label: PlatformsName;
+  size: "small" | "large";
+}
+
+type PlatformsName =
+  | "GitHub"
+  | "Frontend Mentor"
+  | "Twitter"
+  | "LinkedIn"
+  | "YouTube"
+  | "Facebook"
+  | "Twitch"
+  | "Dev.to"
+  | "Codewars"
+  | "freeCodeCamp"
+  | "GitLab"
+  | "Hashnode"
+  | "Stack Overflow";
+
+type PlatformClass =
+  | ""
+  | "GitHub"
+  | "FrontendMentor"
+  | "Twitter"
+  | "LinkedIn"
+  | "YouTube"
+  | "Facebook"
+  | "Twitch"
+  | "Devto"
+  | "Codewars"
+  | "freeCodeCamp"
+  | "GitLab"
+  | "Hashnode"
+  | "StackOverflow";
+
+export { LinkTypes, PlatformsName, PlatformClass };
---
+++ src/components/PreviewLink/index.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import { LinkTypes, PlatformClass } from "./LinkTypes";
+import { useEffect, useState } from "react";
+import style from "./style.module.css";
+
+const PreviewLink: React.FC<LinkTypes> = ({
+  link,
+  label,
+  target = "_blank",
+  size,
+  ...props
+}) => {
+  const [platformClass, setPlatformClass] = useState<PlatformClass>("");
+
+  useEffect(() => {
+    if (label === "Dev.to") {
+      setPlatformClass("Devto");
+    } else if (label === "Frontend Mentor") {
+      setPlatformClass("FrontendMentor");
+    } else if (label === "Stack Overflow") {
+      setPlatformClass("StackOverflow");
+    } else {
+      setPlatformClass(label);
+    }
+  }, [label]);
+
+  return (
+    <a
+      className={`${
+        size === "large" ? "h-14" : "h-11"
+      } flex items-center border-none justify-between text-white pl-[54px] BodyM w-60 p-4 border rounded-lg ${
+        style[platformClass]
+      }`}
+      href={link}
+      aria-label=""
+      target={target}
+      {...props}
+    >
+      {label}
+      <Image alt="" src="/assets/icon-arrow-right.svg" width="16" height="16" />
+    </a>
+  );
+};
+
+export default PreviewLink;
