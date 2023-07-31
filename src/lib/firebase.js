65d692c7cfb4e436b2df02714a8dd5f607e25c80
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-07-31 19:54:31 -0300

    feat: firebase storage config

---
+++ src/lib/firebase.js
@@ -0,0 +1,26 @@
+const { initializeApp } = require("firebase/app")
+const { getStorage } = require("firebase/storage")
+
+const apiKey = process.env.API_KEY;
+const authDomain = process.env.AUTH_DOMAIN;
+const projectId = process.env.PROJECT_ID
+const storageBucket = process.env.STORAGE_BUCKET
+const messagingSenderId = process.env.MESSAGING_SENDER_ID
+const appId = process.env.APP_ID
+const measurementId = process.env.MEASUREMENT_ID
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId
+}
+
+const app = initializeApp(firebaseConfig)
+
+const storage = getStorage(app)
+
+module.exports = storage
