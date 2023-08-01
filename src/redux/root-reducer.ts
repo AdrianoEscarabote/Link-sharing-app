b408a052f7b2be2d075540c2a400b31df89e1a51
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-08-01 18:19:09 -0300

    feat: userLinksSlice in root-reducer

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import profileDataSlice from "./userProfileData/reducer";
+import userLinksSlice from "./userLinks/reducer";
 
 const rootReducer = combineReducers({
   profileDataSlice,
+  userLinksSlice,
 });
 
 export default rootReducer;
