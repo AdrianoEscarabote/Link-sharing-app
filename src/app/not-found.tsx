1e1dd188dc2f593c9d9572e20333924b0e2cdb2f
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-08-07 12:18:53 -0300

    feat: not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import useUserAuthenticated from "@/hooks/useUserAuthenticated";
+import Image from "next/image";
+
+const CustomErrorPage = () => {
+  useUserAuthenticated();
+
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <Image
+          className="mb-8"
+          src={"/assets/logo-devlinks-large.svg"}
+          alt=""
+          width={180}
+          height={70}
+        />
+        <h1 className="HeadingM text-dark_purple">
+          The page you are looking for does not exist
+        </h1>
+        <h2 className="HeadingS">Returning to the home page!</h2>
+      </div>
+    </main>
+  );
+};
+
+export default CustomErrorPage;
