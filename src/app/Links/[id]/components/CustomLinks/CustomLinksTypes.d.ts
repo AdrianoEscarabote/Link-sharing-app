9871825a58c981ba6c2fdb8eb50503e186652f85
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-08-08 19:28:56 -0300

    deleting old files

--- src/app/Links/[id]/components/CustomLinks/CustomLinksTypes.d.ts
+++
@@ -1,9 +0,0 @@
-import { PlatformsName } from "@/redux/root-reducer-types";
-
-interface linkSelectorProps {
-  id: number;
-  link: string;
-  platformName: PlatformsName | string;
-}
-
-export { linkSelectorProps };
