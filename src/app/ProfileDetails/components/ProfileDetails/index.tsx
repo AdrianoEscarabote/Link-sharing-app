6efbe985e0e13b3ae812a247f0a7415b308577c9
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-08-09 12:40:23 -0300

    adding the signout component

--- src/app/ProfileDetails/components/ProfileDetails/index.tsx
+++ src/app/ProfileDetails/components/ProfileDetails/index.tsx
@@ -1,11 +1,32 @@
+import ButtonPrimary from "@/components/ButtonPrimary";
+import SignoutComponent from "@/components/Signout";
+import Image from "next/image";
+import { useState } from "react";
+
 const ProfileDetails = () => {
+  const [showSignoutComponent, setShowSignoutComponent] =
+    useState<boolean>(false);
   return (
-    <div>
-      <h1 className="HeadingM text-almost_dark">Profile Details</h1>
-      <p className="BodyM text-gray">
-        Add your details to create a personal touch to your profile.
-      </p>
-    </div>
+    <>
+      <div className="w-full flex items-center justify-between">
+        <div>
+          <h1 className="HeadingM text-almost_dark">Profile Details</h1>
+          <p className="BodyM text-gray">
+            Add your details to create a personal touch to your profile.
+          </p>
+        </div>
+        <button
+          onClick={() => setShowSignoutComponent(true)}
+          className="flex items-center gap-2 h-10 rounded-full"
+        >
+          <span className="BodyM text-red">Logout</span>
+          <Image src="/assets/logout-icon.svg" width={30} height={30} alt="" />
+        </button>
+      </div>
+      {showSignoutComponent ? (
+        <SignoutComponent cancelSignout={setShowSignoutComponent} />
+      ) : null}
+    </>
   );
 };
 
