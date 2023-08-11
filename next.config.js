e991a766144e07c0d71fa9ee5db079a78aa2a84a
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-08-10 22:33:44 -0300

    changing react strict mode to false

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import("next").NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
