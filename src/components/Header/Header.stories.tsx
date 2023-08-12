bf9f7e4011a1700d94dca91141b7ad1afcdc6ba9
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-08-11 22:58:09 -0300

    feat: header & AlertModal stories

---
+++ src/components/Header/Header.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Header from ".";
+
+export default {
+  title: "components/shared/Header",
+  component: Header,
+  tags: ["autodocs"],
+} as Meta;
+
+export const Primary: StoryObj = {};
---
+++ src/components/ModalAlert/modal.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from "@storybook/react";
+import ModalLogin from ".";
+
+export default {
+  title: "components/ModalLogin",
+  component: ModalLogin,
+  tags: ["autodocs"],
+} as Meta;
+
+export const Primary: StoryObj = {};
