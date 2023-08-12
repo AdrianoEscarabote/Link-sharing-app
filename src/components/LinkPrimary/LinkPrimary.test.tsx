3afe27eb27f7833956f15b58aaf29c55b91884da
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-08-11 22:46:16 -0300

    feat: link primary & secondary tests

---
+++ src/components/LinkPrimary/LinkPrimary.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import LinkPrimary from ".";
+
+describe("Link Primary test", () => {
+  it("should render correctly", () => {
+    render(<LinkPrimary disabled={false} label="Link" href="/" />);
+  });
+
+  it("should pass props correctly", () => {
+    render(<LinkPrimary label="Link Primary" href="/" disabled={false} />);
+
+    const element = screen.getByText("Link Primary");
+
+    expect(element).toBeInTheDocument();
+  });
+});
---
+++ src/components/LinkSecondary/LinkSecondary.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import LinkSecondary from ".";
+
+describe("Link Secondary Component", () => {
+  it("should render correctly", () => {
+    render(<LinkSecondary href="/" disabled={false} label="Link Secondary" />);
+  });
+
+  it("should pass props correctly", () => {
+    render(<LinkSecondary href="/" disabled={false} label="Link Secondary" />);
+
+    const element = screen.getByText("Link Secondary");
+
+    expect(element).toBeInTheDocument();
+  });
+});
