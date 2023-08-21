6f4131db0a7f4ebd61cf00539e60566e374815e8
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-08-21 11:09:44 -0300

    feat: add main function

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,64 @@
 // database
 import { startApp } from "./config/database"
-startApp()
 
 // express
 import express from "express"
 import { Request, Response, NextFunction } from "express"
 
 // env
-import * as env from "dotenv"
-env.config()
+import { config } from "dotenv"
 
 // cookie-parser
 import cookieParser from "cookie-parser"
 
 // cors
 import cors from "cors"
-const app = express()
-// configures the color to allow only one origin
+
+// configures the cors to allow only one origin
 const corsOptions = {
   credentials: true,
   origin: "https://link-sharing-app-alpha.vercel.app",
 }
 
-app.use(function (req: Request, res: Response, next: NextFunction) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://link-sharing-app-alpha.vercel.app",
-  )
-  res.header("Access-Control-Allow-Credentials", "true")
-  next()
-})
-
-app.use(cors(corsOptions))
-app.use(cookieParser())
-app.use(express.json())
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world!")
-})
-
 // auth
 import authRouter from "./routes/authRouter"
-app.use("/auth", authRouter)
 
 // profile
 import profileRouter from "./routes/profileRouter"
-app.use("/profile", profileRouter)
 
-app.listen(4002, () => {
-  console.log("Servidor iniciado em http://localhost:4002")
-})
+const main = async () => {
+  config()
+
+  const app = express()
+
+  app.use(express.json())
+
+  await startApp()
+
+  app.use(cors(corsOptions))
+  app.use(function (req: Request, res: Response, next: NextFunction) {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://link-sharing-app-alpha.vercel.app",
+    )
+    res.header("Access-Control-Allow-Credentials", "true")
+    next()
+  })
+  app.use(cookieParser())
+
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Hello world!")
+  })
+
+  app.use("/auth", authRouter)
+
+  app.use("/profile", profileRouter)
+
+  const port = 8000
+
+  app.listen(port, () => {
+    console.log("listening on port http://localhost:8000")
+  })
+}
+
+main()
