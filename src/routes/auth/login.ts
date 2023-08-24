9b46c4ea6360b6210adbb3192e7759d8559aee03
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-08-23 22:28:29 -0300

    chore: login and register route

--- src/routes/auth/login.ts
+++ src/routes/auth/login.ts
@@ -1,87 +1,27 @@
 // express
 import express from "express"
-import { Response, Request } from "express"
-
-// Models
-import User from "@/models/User"
-
-// json web token
-import { Secret, sign } from "jsonwebtoken"
-
-// bcrypt
-import { compare } from "bcrypt"
-
 const loginRoute = express.Router()
 
-interface LoginRouteBodyTypes {
-  email: string
-  password: string
-  confirmpassword: string
-}
+// login user controller
+import { LoginUserController } from "@/controllers/login-user/login-user"
+
+// login user repository
+import { MongoLoginUserRepository } from "@/repositories/login-user/mongo-login-user"
 
 // Login User
-loginRoute.post("/", async (req: Request, res: Response) => {
-  const { email, password }: LoginRouteBodyTypes = req.body
+loginRoute.post("/", async (req, res) => {
+  const mongoLoginUserRepository = new MongoLoginUserRepository()
 
-  if (!email) {
-    return res.status(422).json({ msg: "The email is wrong!" })
-  }
+  const loginUserController = new LoginUserController(mongoLoginUserRepository)
 
-  if (!password) {
-    return res.status(422).json({ msg: "The password is wrong!" })
-  }
+  const { body, statusCode } = await loginUserController.handle(
+    {
+      body: req.body,
+    },
+    res,
+  )
 
-  // check if user exists
-  const user = await User.findOne({ email: email })
-
-  if (!user) {
-    return res.status(404).json({ msg: "User does not exist!" })
-  }
-
-  // check if password match
-  const checkPassword = await compare(password, user.password as string)
-
-  if (!checkPassword) {
-    return res.status(422).json({ msg: "Invalid password!" })
-  }
-
-  const expirationDate = new Date()
-  expirationDate.setDate(expirationDate.getDate() + 30)
-
-  try {
-    const secret = process.env.SECRET as Secret
-
-    const token = sign(
-      {
-        id: user._id,
-      },
-      secret,
-    )
-
-    res.cookie("id", user._id, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      expires: expirationDate,
-    })
-
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      expires: expirationDate,
-    })
-
-    return res.status(200).json({
-      msg: "Authentication successful!",
-      _id: user._id,
-      token: token,
-    })
-  } catch (e) {
-    return res.send(500).json({
-      mesg: "There was a server error, please try again later!",
-    })
-  }
+  res.status(statusCode).send(body)
 })
 
 export default loginRoute
--- src/routes/auth/register.ts
+++ src/routes/auth/register.ts
@@ -1,94 +1,28 @@
 // express
 import express from "express"
-import { Response, Request } from "express"
-
-// bcrypt
-import { hash, genSalt } from "bcrypt"
-
-// Json web token
-import { sign, Secret } from "jsonwebtoken"
-
-// Models
-import User from "@/models/User"
-
-// middlewares
-import checkToken from "@/middlewares/checktoken"
-
 const RegisterRouter = express.Router()
 
-interface RegisterBodyTypes {
-  email: string
-  password: string
-  confirmpassword: string
-}
+// Register user repository
+import { MongoRegisterUserRepository } from "@/repositories/register-user/mongo-register-user"
 
-RegisterRouter.post("/", checkToken, async (req: Request, res: Response) => {
-  const { confirmpassword, email, password }: RegisterBodyTypes = req.body
+// Register user controller
+import { RegisterUserController } from "@/controllers/register-user/register-user"
 
-  if (!email) {
-    return res.status(422).json({ msg: "email is required!" })
-  }
+RegisterRouter.post("/", async (req, res) => {
+  const mongoRegisterUserRepository = new MongoRegisterUserRepository()
 
-  if (!password) {
-    return res.status(422).json({ msg: "password is required!" })
-  }
+  const registerUserController = new RegisterUserController(
+    mongoRegisterUserRepository,
+  )
 
-  if (password != confirmpassword) {
-    return res
-      .status(422)
-      .json({ msg: "Password and confirmation must match!" })
-  }
+  const { body, statusCode } = await registerUserController.handle(
+    {
+      body: req.body,
+    },
+    res,
+  )
 
-  // check if user exists
-  const userExists = await User.findOne({ email: email })
-
-  if (userExists) {
-    return res.status(422).json({ msg: "Please use another email!" })
-  }
-
-  // create password
-  const salt = await genSalt(12)
-  const passwordHash = await hash(password, salt)
-
-  // create user
-  const user = new User({
-    email,
-    password: passwordHash,
-  })
-
-  const expirationDate = new Date()
-  expirationDate.setDate(expirationDate.getDate() + 30)
-
-  try {
-    await user.save()
-
-    const secret = process.env.SECRET as Secret
-
-    const token = sign(
-      {
-        id: user._id,
-      },
-      secret,
-    )
-
-    res.cookie("id", user._id, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      expires: expirationDate,
-    })
-
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      expires: expirationDate,
-    })
-
-    res.status(201).json({ success: true, _id: user._id })
-  } catch (error) {
-    res.status(500).json({ msg: error })
-  }
+  res.status(statusCode).send(body)
 })
 
 export default RegisterRouter
