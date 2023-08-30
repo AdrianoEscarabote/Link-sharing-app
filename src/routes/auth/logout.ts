4e40cf04b0a41aece95d5669cb3a0c14ae64435b
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-08-30 11:20:46 -0300

    chore: add clearCookie to logout route

--- src/routes/auth/logout.ts
+++ src/routes/auth/logout.ts
@@ -3,25 +3,45 @@ import express, { Response, Request } from "express"
 
 // middlewares
 import checkToken from "@/middlewares/checktoken"
+import { MongoClient } from "@/database/mongo"
+import { MongoUser } from "@/repositories/mongo-protocols"
+import { ObjectId } from "mongodb"
 
 const logoutRoute = express.Router()
 
 logoutRoute.post("/", checkToken, async (req: Request, res: Response) => {
-  const id = req.cookies.id
-  const token = req.cookies.token
+  try {
+    const id = req.cookies.id
 
-  if (!id || !token) {
-    return res.status(400).json({
-      msg: "id or token not found!",
+    const user = await MongoClient.db.collection<MongoUser>("users").findOne({
+      _id: new ObjectId(id),
     })
+
+    if (!user) {
+      return res.status(400).json({
+        msg: "user is not present in the database!",
+      })
+    }
+
+    res.clearCookie("id", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: "/",
+    })
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: "/",
+    })
+
+    return res.status(200).json({
+      msg: "success!",
+    })
+  } catch (error) {
+    return res.status(500).json({ msg: "server error!" })
   }
-
-  res.clearCookie("id")
-  res.clearCookie("token")
-
-  return res.status(200).json({
-    msg: "success!",
-  })
 })
 
 export default logoutRoute
