4629af0b1301fb12a209d8184b72e1ad72bccdde
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-08-31 11:45:34 -0300

    chore(eslint): add eslint-config-next and eslint-plugin-import-sort with config files

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,26 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+];
+
+export default eslintConfig;
