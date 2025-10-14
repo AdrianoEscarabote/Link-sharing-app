146ef5446a00f844685a5ffa69927ddbec1cc076
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-10-14 11:49:01 -0300

    chore(tests): add eslint-disable for no-explicit-any in HeaderPreview and PreviewCard tests

--- frontend/src/app/Preview/[...username]/components/PreviewCard/PreviewCard.test.tsx
+++ frontend/src/app/Preview/[...username]/components/PreviewCard/PreviewCard.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureMockstore from "redux-mock-store";
