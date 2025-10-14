676dc75c9ba7905ca9e6e61ff42e20d5be00f91f
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-10-14 10:43:20 -0300

    chore(playwright): update test directory path in configuration

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./tests/e2e",
+  testDir: "./e2e",
   fullyParallel: true,
   retries: 1,
   reporter: [["list"], ["html", { outputFolder: "playwright-report" }]],
